8c623063378749bfffc48f01b69ae1bf7aec56f9
Alejandro Avilez <aleantonio656@gmail.com>
2022-10-27 22:40:17 -0600

    Adding new SideBar
    New Sidebar pre design added to all views

--- inventory-app/src/app/components/side-nav/nav-data.ts
+++ inventory-app/src/app/components/side-nav/nav-data.ts
@@ -4,4 +4,20 @@ export const navbarData = [
     icon: 'far fa-list-alt',
     label: 'Products',
   },
+  {
+    routeLink: '/categories',
+    icon: 'fas fa-tags',
+    label: 'Categories',
+  },
+  {
+    routeLink: '/users',
+    icon: 'fas fa-users',
+    label: 'Users',
+  },
+  {
+    logout: 'logout',
+    routeLink: '/login',
+    icon: 'fas fa-sign-out-alt',
+    label: 'Logout',
+  },
 ];
